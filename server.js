7b63ad85561eb073ce7aea0f677e2a6b7c6acbbd
Erwin <ecofreandaur@gmail.com>
2021-07-15 14:48:36 -0400

    ruta con pass

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 ////servidor con express
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-require('dotenv').config();
+require("dotenv").config();
 //// parse json para el body
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 //importar las rutas
-app.use(require('./server/config/rutas'));
+app.use(require("./server/config/rutas"));
 
 //ruta de prueba///
 app.get("/api", (req, res) => {
     return res.status(200).json({
         ok: true,
-        message: 'API de Prueba'
-    })
+        message: "API de Prueba",
+    });
 });
 
 app.listen(process.env.PORT, () => {
-    console.log('conectado al puerto ' + process.env.PORT);
+    console.log("conectado al puerto " + process.env.PORT);
 });
